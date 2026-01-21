5d67a52cd27b9a31a1568a733277cb4284e12fb1
imddoy <imddoy@duksung.ac.kr>
2026-01-21 11:00:44 +0900

    HN-36-chore/prisma generate 폴더 위치 변경

--- hanaro-pass/lib/prisma.ts
+++ hanaro-pass/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 import "dotenv/config";
 
 const adapter = new PrismaMariaDb({
