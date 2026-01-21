d9c6c550f386313aa23e499661049657e5fc8098
imddoy <imddoy@duksung.ac.kr>
2026-01-21 09:14:52 +0900

    HN-46-feat/스토리북 router 마운트 추가

--- hanaro-pass/.storybook/preview.ts
+++ hanaro-pass/.storybook/preview.ts
@@ -1,5 +1,5 @@
-import type { Preview } from "@storybook/nextjs-vite";
-import "../app/globals.css";
+import type { Preview } from '@storybook/nextjs-vite';
+import '../app/globals.css';
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +8,11 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    nextjs: {
+      appDirectory: true,
+      pathname: '/',
+      query: {},
     },
   },
 };
