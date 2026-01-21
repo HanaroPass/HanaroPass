736eed6296d4e5e14defeef320cd850bfb2c7ed5
kimm00 <kimdoyi02@naver.com>
2026-01-21 11:04:03 +0900

    HN-59-feat/bottomsheet 토글 및 버튼 추가

---
+++ hanaro-pass/app/map/components/bottomsheet.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useState } from "react";
+
+type FilterType = "language" | "department" | null;
+
+export default function BottomSheet({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
+  const [active, setActive] = useState<FilterType>(null);
+  const basePill =
+  "rounded-full px-5 py-2 text-sm font-medium border transition";
+  const inactivePill = "bg-white border-gray-300 text-gray-700";
+  const activePill = "bg-green-ez border-green-ez text-white";
+
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerContent className="h-[75vh] p-0">
+        <DrawerHeader>
+          <DrawerTitle>
+            <VisuallyHidden>병원 필터</VisuallyHidden>
+          </DrawerTitle>
+        </DrawerHeader>
+
+        <div className="sticky top-0 z-[60] bg-white px-4 py-3 flex gap-2 border-b">
+          <button
+            className={`${basePill} ${
+              active === "language" ? activePill : inactivePill
+            }`}
+            onClick={() => setActive("language")}
+          >
+            소통 가능 언어
+          </button>
+
+          <button
+            className={`${basePill} ${
+              active === "department" ? activePill : inactivePill
+            }`}
+            onClick={() => setActive("department")}
+          >
+            진료과목
+          </button>
+        </div>
+
+        <div className="flex-1 overflow-y-auto px-6 pb-6">
+          <h2 className="text-lg font-bold">어쩌구 바른 내과</h2>
+          <p className="text-sm text-muted-foreground">
+            서울시 강남구 테헤란로 123
+          </p>
+
+        <Button className="
+            mt-4
+            w-full
+            rounded-10
+            bg-green-ez
+            py-6
+            text-base
+            font-semibold
+            text-white
+            hover:bg-green-ez/90
+          "
+        >
+          AI에게 나에게 맞는 병원 물어보기
+        </Button>
+
+          <div className="h-[800px]" />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
