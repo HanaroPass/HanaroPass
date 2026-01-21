c163735159fab2054ee359caacf1befc587fb9c2
imddoy <imddoy@duksung.ac.kr>
2026-01-21 13:36:51 +0900

    HN-36-feat/쿠폰 리스트 UI 구현

--- hanaro-pass/app/benefits/page.tsx
+++ hanaro-pass/app/benefits/page.tsx
@@ -1,6 +1,7 @@
 import { Search } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import Coupon from './components/coupon/Coupon';
+import { mockCoupons } from './mock/mockCoupon';
 
 function page() {
   return (
@@ -9,7 +10,19 @@ function page() {
         <Search />
         <Input placeholder="쿠폰 검색" className="border-none outline-none" />
       </div>
-      <Coupon />
+      <div className="flex flex-col gap-3">
+        {mockCoupons.map((coupon) => (
+          <Coupon
+            key={coupon.id}
+            brandName={coupon.brandName}
+            tag={coupon.tag}
+            category={coupon.category}
+            discount={coupon.discount}
+            description={coupon.description}
+            brandPic={coupon.brandPic}
+          />
+        ))}
+      </div>{' '}
     </div>
   );
 }
