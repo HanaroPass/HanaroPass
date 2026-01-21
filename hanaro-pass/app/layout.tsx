b0a68725aa5ec22d9ffd89fa785e85255fa084d6
imddoy <imddoy@duksung.ac.kr>
2026-01-21 10:30:17 +0900

    HN-46-fix/코드래빗 리뷰 반영

--- hanaro-pass/app/layout.tsx
+++ hanaro-pass/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Hanaro Pass',
   description:
-    ' 한국 생활, 하나로 끝! 모든 방한 외국인을 위한 한국 여정 금융 동반 서비스, 하나로패스',
+    '한국 생활, 하나로 끝! 모든 방한 외국인을 위한 한국 여정 금융 동반 서비스, 하나로패스',
 };
 
 export default function RootLayout({
