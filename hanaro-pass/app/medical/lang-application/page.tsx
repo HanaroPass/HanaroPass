595666dcc81f8102e26358fc29fc1e00d0c849a6
KelseyKwon <nykwon7777@gmail.com>
2026-01-21 13:26:00 +0900

    HN-52-feat/hospital UI : 병원 언어 등록 페이지 초기 설정

---
+++ hanaro-pass/app/medical/lang-application/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/header/Header';
+
+export default function HospitalLanguageApplicationPage() {
+  return (
+    <div className="app-shell bg-white shadow-lg">
+      <div className="app-layout">
+        <Header title="병원 관리자 등록" leftType="back" />
+      </div>
+    </div>
+  );
+}
