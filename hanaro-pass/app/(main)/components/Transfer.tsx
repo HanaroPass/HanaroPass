6fb1f6df8d1f32194e8f1079e92173294dee5d69
imddoy <imddoy@duksung.ac.kr>
2026-01-22 20:16:45 +0900

    HN-71-feat/조회이체 페이지 UI 구현

--- hanaro-pass/app/(main)/components/Transfer.tsx
+++ hanaro-pass/app/(main)/components/Transfer.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import AccountSummary from './AccountSummary';
 import MenuList from './MenuList';
 
 function Transfer() {
   return (
-    <div>
+    <div className="flex flex-col gap-5">
+      <AccountSummary />
       <MenuList type="transfer" />
+      <Link
+        href="https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1508308_115157.jsp"
+        className="block w-full"
+        target="blank"
+      >
+        <div className="relative aspect-340/139 w-full overflow-hidden">
+          <Image
+            src="/images/main/ad-savings.png"
+            alt="하나 EZ 적금"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+      </Link>
     </div>
   );
 }
