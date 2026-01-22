fc6d215206fecd9faff61f820c061978b75cae94
imddoy <imddoy@duksung.ac.kr>
2026-01-23 08:40:28 +0900

    HN-69-feat/쿠폰 추천 리스트 구현

--- hanaro-pass/app/(main)/components/Pay.tsx
+++ hanaro-pass/app/(main)/components/Pay.tsx
@@ -1,7 +1,13 @@
+import CouponList from './CouponList';
 import MenuList from './MenuList';
 
 function Pay() {
-  return <MenuList type="pay" />;
+  return (
+    <div className="flex flex-col gap-5">
+      <CouponList />
+      <MenuList type="pay" />
+    </div>
+  );
 }
 
 export default Pay;
