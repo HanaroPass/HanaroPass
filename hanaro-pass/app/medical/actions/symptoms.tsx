f9d0e92480e4177ee199f43f7e1039c6022f82ae
sonsy__20 <s3young.dev@gmail.com>
2026-01-22 18:38:04 +0900

    HN-56-feat/증상 입력 UI 구현, 증상/시술 라디오그룹 스토리북 작성

--- hanaro-pass/app/medical/actions/symptoms.tsx
+++ hanaro-pass/app/medical/actions/symptoms.tsx
@@ -1,5 +1,13 @@
 'use server';
 
+export type outputType = {
+  주요_증상?: string[];
+  발생_시점?: string;
+  희망_시술?: string[];
+  요청_사유?: string[];
+  응급_여부: '낮음' | '중간' | '높음' | '매우 높음';
+  번역_내용: string;
+};
 export async function postOpenAI(url: string, body: string) {
   const apiKey = process.env.OPENAI_API_KEY;
   if (!apiKey) throw new Error('OPEN AI API 키가 없습니다.');
@@ -32,7 +40,7 @@ export async function postOpenAI(url: string, body: string) {
 export async function postSymptomForm(formData: FormData) {
   const symptomPrompt = `
   당신은 의료 보조 AI입니다.
-  사용자가 입력한 text는 아마 한국어가 아닌 다른 언어일 수 있습니다. 한국어로 번역 및 정리까지 해주세요.
+  사용자가 입력한 text는 아마 한국어가 아닌 다른 언어일 수 있습니다. 한국어로 번역 및 요약해주세요. 반드시 다른 설명 없이 번역한 문장만 작성하세요.
   사용자가 입력한 text와 이미지를 통해 주요 증상, 발생 시점, 응급 여부를 알 수 있다면 짚어주셔야 합니다.
   이외의 다른 말은 작성하지마세요. 반드시 아래 형식의 JSON으로 출력해주세요. 반드시 설명 문장, 마크다운, 코드블록 없이! 순수 JSON만 반환해주세요.
   {
@@ -53,7 +61,7 @@ export async function postSymptomForm(formData: FormData) {
   `;
   const surgeryPrompt = `
   당신은 성형 및 뷰티 의료 보조 AI입니다.
-  사용자가 입력한 text는 아마 한국어가 아닌 다른 언어일 수 있습니다. 한국어로 번역 및 정리까지 해주세요.
+  사용자가 입력한 text는 아마 한국어가 아닌 다른 언어일 수 있습니다. 한국어로 번역 및 요약해주세요. 반드시 다른 설명 없이 번역한 문장만 작성하세요.
   사용자가 입력한 text와 이미지를 통해 희망 시술, 요청 사유, 응급 여부를 알 수 있다면 짚어주셔야 합니다.
   이외의 다른 말은 작성하지마세요. 반드시 아래 형식의 JSON으로 출력해주세요. 반드시 설명 문장, 마크다운, 코드블록 없이! 순수 JSON만 반환해주세요.
   {
@@ -75,7 +83,8 @@ export async function postSymptomForm(formData: FormData) {
   const prompt = type === 'SYMPTOM' ? symptomPrompt : surgeryPrompt;
   const description = formData.get('description') as string;
 
-  if (!description.trim()) throw new Error('필요한 값이 없습니다');
+  if (!description || !description.trim())
+    throw new Error('필요한 값이 없습니다');
 
   const images = formData.getAll('images') as File[];
   let imagesToBase64 = [];
@@ -115,11 +124,12 @@ export async function postSymptomForm(formData: FormData) {
     throw new Error('AI 분석 처리 중 오류가 발생했습니다.');
   console.log(output);
 
-  return await parseOutput(output);
+  return output;
 }
 
 export async function getTTS(description: string) {
-  if (!description.trim()) throw new Error('필요한 값이 없습니다.');
+  if (!description || !description.trim())
+    throw new Error('필요한 값이 없습니다.');
   const body = JSON.stringify({
     model: 'gpt-4o-mini-tts',
     voice: 'alloy',
@@ -133,14 +143,7 @@ export async function getTTS(description: string) {
 }
 
 export async function parseOutput(output: string) {
-  let result: {
-    주요_증상?: string[];
-    발생_시점?: string;
-    희망_시술?: string[];
-    요청_사유?: string[];
-    응급_여부: '낮음' | '중간' | '높음' | '매우 높음';
-    번역_내용: string;
-  };
+  let result: outputType;
   try {
     result = JSON.parse(output);
   } catch {
---
+++ hanaro-pass/app/medical/symptoms/analyze/page.tsx
@@ -0,0 +1,150 @@
+'use client';
+import { Loader, PlusIcon, XIcon } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { type ChangeEvent, useState } from 'react';
+import ActionButton from '@/components/ui/ActionButton';
+import { postSymptomForm } from '../../actions/symptoms';
+import SymptomRadioGroup from '../../components/symptom/SymptomRadioGroup';
+
+export default function SymptomAnalyzePage() {
+  const [images, setImages] = useState<File[]>([]);
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isImageCntOK, setImageCntOK] = useState(true);
+
+  const handleImages = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const newImages = Array.from(e.target.files);
+
+      if (images.length + newImages.length > 3) {
+        setImageCntOK(false);
+        setImages(newImages.slice(0, 3));
+      } else setImageCntOK(true);
+      const totalImages = [...images, ...newImages.slice(0, 3 - images.length)];
+      setImages(totalImages);
+
+      const urls = totalImages.map((i) => URL.createObjectURL(i));
+      setImageUrls(urls);
+    }
+  };
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+    const response = await postSymptomForm(formData);
+
+    setLoading(false);
+    console.log(response);
+    localStorage.setItem('symptom-result', response);
+    router.push('/medical/symptoms/result');
+  };
+
+  return (
+    <>
+      {isLoading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="flex h-44 w-44 flex-col items-center justify-center gap-2 rounded-4xl bg-black/60 text-center font-medium text-sm text-white">
+            <div>AI가 진단하고 있습니다.</div>
+            <div>잠시만 기다려주세요.</div>
+            <Loader className="h-8 w-8 animate-spin text-white" />
+          </div>
+        </div>
+      )}
+      <div className="px-8">
+        <div className="my-7 h-48 rounded-xl bg-green-300 p-5">
+          <div className="pb-2 font-semibold text-sm text-teal-600 leading-5">
+            혹시 작성이 어렵나요?
+          </div>
+          <div className="mb-2 h-14 w-full rounded-lg bg-white-ez pt-3 pl-3">
+            <div className="justify-center pb-[7px] font-medium text-black_900 text-xs leading-4">
+              TIP 01. 증상이라면
+            </div>
+            <div className="justify-center font-medium text-[8px] text-black_900 leading-3">
+              언제부터 증상이 있었는지, 어느 부위가 아픈지를 중심으로
+              적어주세요.
+            </div>
+          </div>
+          <div className="h-14 w-full rounded-lg bg-white-ez pt-3 pl-3">
+            <div className="justify-center pb-[7px] font-medium text-black-900 text-xs leading-4">
+              TIP 02. 시술이라면
+            </div>
+            <div className="justify-center font-medium text-[8px] text-black-900 leading-3">
+              어떤 시술을 받고 싶은지, 희망 날짜나 목적이 있다면 함께
+              적어주세요.
+            </div>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="font-medium text-base leading-6">
+            증상/시술을 선택해주세요.<span className="text-orange-400">*</span>
+          </div>
+          <SymptomRadioGroup />
+          <div className="pt-5 pb-3 font-medium text-base leading-6">
+            구체적인 설명을 입력해주세요.
+            <span className="text-orange-400">*</span>
+          </div>
+          <textarea
+            className="h-64 w-full resize-none rounded-[10px] bg-gray-200 px-4 py-3 placeholder:font-normal placeholder:text-black-800 placeholder:leading-6"
+            name="description"
+            rows={6}
+            required
+            placeholder={`ex)\n어제부터 갑자기 배가 아프기 시작했어요.\n누가 배를 콕콕콕 찌르는 것 같이 아파요.`}
+          />
+          <div className="pt-5 font-medium text-base leading-6">
+            관련 사진을 첨부해주세요. ({images.length}/3)
+          </div>
+          {!isImageCntOK && (
+            <div className="text-red-700">
+              사진은 3개까지만 첨부할 수 있습니다.
+            </div>
+          )}
+          <div className="no-scrollbar flex overflow-x-auto pt-3">
+            {images.length < 3 ? (
+              <label>
+                <input
+                  type="file"
+                  name="images"
+                  multiple
+                  accept="image/*"
+                  onChange={handleImages}
+                  className="hidden"
+                />
+                <div className="mr-3 mb-11 flex h-28 w-28 items-center justify-center border-2 border-gray-100 bg-gray-200">
+                  <PlusIcon className="h-11 w-11 text-gray-400" />
+                </div>
+              </label>
+            ) : (
+              <div className="mr-3 mb-11 flex h-28 w-28 shrink-0 items-center justify-center border-2 border-gray-100 bg-gray-200">
+                <XIcon
+                  onClick={() => {
+                    setImages([]);
+                    setImageUrls([]);
+                  }}
+                  className="h-11 w-11 text-gray-400"
+                />
+              </div>
+            )}
+            {imageUrls.map((url) => (
+              <div
+                className="relative mr-3 h-28 w-28 shrink-0 overflow-hidden rounded-[10px]"
+                key={url}
+              >
+                <Image src={url} alt="preview" fill className="object-cover" />
+              </div>
+            ))}
+          </div>
+          <ActionButton
+            text="다 입력했어요"
+            className="mb-9"
+            onClick={() => {}}
+          />
+        </form>
+      </div>
+    </>
+  );
+}
