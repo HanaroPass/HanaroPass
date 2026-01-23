de46f154102f38d6734cf593fdd807e706aa5294
imddoy <imddoy@duksung.ac.kr>
2026-01-23 18:25:41 +0900

    HN-94-feat/혜택 헤더 수정

--- hanaro-pass/app/coupons/(coupon-header)/[id]/page.tsx
+++ hanaro-pass/app/coupons/(coupon-header)/[id]/page.tsx
@@ -1,7 +1,7 @@
-import CouponDetail from '../components/coupon/CouponDetail';
+import CouponDetail from '../../components/coupon/CouponDetail';
 
 type PageProps = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 export default async function Page({ params }: PageProps) {
