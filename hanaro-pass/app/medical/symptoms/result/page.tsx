2dde8742e580d94857e4a9d9e0408a6574c8bd98
sonsy__20 <s3young.dev@gmail.com>
2026-01-24 17:04:07 +0900

    HN-98-feat/병원 결과 로직 정리

--- hanaro-pass/app/medical/symptoms/result/page.tsx
+++ hanaro-pass/app/medical/symptoms/result/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Copy } from 'lucide-react';
+import { Check, Copy, Loader } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import ActionButton from '@/components/ui/ActionButton';
 import AIResultIcon from '../../../../components/ui/AIResultIcon';
@@ -7,11 +7,15 @@ import { getTTS, type outputType, parseOutput } from '../../actions/symptoms';
 import HospitalGuide from '../../components/languageRegistration/HospitalGuide';
 import EmergencyBadge from '../../components/symptom/EmergencyBadge';
 import Symptom from '../../components/symptom/Symptom';
+import useSymptomResult from '../../hooks/useSymptomResult';
 
 export default function SymptomResultPage() {
   const [writtenSymptom, setWrittenSymptom] = useState('');
   const [type, setType] = useState<'SYMPTOM' | 'PROCEDURE'>('SYMPTOM');
   const [result, setResult] = useState<outputType>();
+  const [copied, setCopied] = useState(false);
+
+  const { isLoading, handleSubmit } = useSymptomResult();
 
   useEffect(() => {
     const parse = async () => {
@@ -33,6 +37,23 @@ export default function SymptomResultPage() {
     audio.play();
   };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(result?.번역_내용 || '');
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
+  const handleResubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    await handleSubmit(e, true);
+    setWrittenSymptom(localStorage.getItem('written-symptom') as string);
+    const data = localStorage.getItem('symptom-result');
+    if (data) {
+      const result = await parseOutput(data);
+      setResult(result);
+      if (result?.주요_증상 || result?.발생_시점) setType('SYMPTOM');
+      else setType('PROCEDURE');
+    }
+  };
   return (
     <div className="px-6">
       <div className="mt-4 flex justify-center">
@@ -41,8 +62,17 @@ export default function SymptomResultPage() {
       <h1 className="mt-3 text-center font-semibold text-2xl text-black-900 leading-8 tracking-tight">
         AI가 {type === 'SYMPTOM' ? '증상' : '시술'}을 정리했어요.
       </h1>
+
       <div className="mt-4 h-1.5 w-96 border-gray-100 border-t" />
-      {type === 'SYMPTOM' ? (
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <div className="flex h-58 w-82 flex-col items-center justify-center gap-2 rounded-4xl bg-[#F9FAFB] text-center font-medium text-green-ez text-sm">
+            <Loader className="mb-4 h-8 w-8 animate-spin text-green-ez" />
+            <div>AI가 진단하고 있습니다.</div>
+            <div>잠시만 기다려주세요.</div>
+          </div>
+        </div>
+      ) : type === 'SYMPTOM' ? (
         <>
           <div className="my-3 ml-2 text-black-800 text-sm">AI 요약 진단</div>
           <div className="w-full rounded-2xl bg-gray-200 p-6 text-black-800 text-sm">
@@ -80,7 +110,7 @@ export default function SymptomResultPage() {
             </div>
             <div className="mt-4">요청 사유</div>
             <div className="mt-2 font-medium text-gray-900">
-              {result?.요청_사유}
+              {result?.요청_사유?.join(', ')}
             </div>
             <div className="mt-4">응급 여부</div>
             <div className="mt-2">
@@ -90,23 +120,29 @@ export default function SymptomResultPage() {
         </>
       )}
       <div className="mt-5 h-1.5 w-96 border-gray-100 border-t" />
-
       <div className="mt-4 text-black-800 text-sm">AI 작성 내용</div>
-      <textarea
-        defaultValue={writtenSymptom}
-        className="black-800 mt-3 h-35 w-full resize-none rounded-2xl bg-gray-200 p-4 text-sm"
-      ></textarea>
-      <ActionButton
-        onClick={() => {}}
-        text="다시 번역하기"
-        invert={true}
-        className="mt-5"
-      />
+      <form onSubmit={handleResubmit} className="mt-5">
+        <input name="type" className="hidden" defaultValue={type} />
+        <textarea
+          defaultValue={writtenSymptom}
+          className="black-800 mt-3 h-35 w-full resize-none rounded-2xl bg-gray-200 p-4 text-sm"
+          name="description"
+        />
+        <ActionButton onClick={() => {}} text="다시 번역하기" invert={true} />
+      </form>
       <div className="mt-7 text-black-800 text-sm">AI 번역 내용</div>
       <div className="relative mt-3 mb-7 w-full rounded-2xl bg-gray-200 p-6 text-black-800 text-sm">
-        <button className="absolute top-2 right-2 text-hana-green">
-          <Copy className="h-4.5 w-4.5" />
+        <button
+          onClick={handleCopy}
+          className="absolute top-2 right-2 text-hana-green"
+        >
+          {copied ? (
+            <Check className="h-4.5 w-4.5 text-green-600" />
+          ) : (
+            <Copy className="h-4.5 w-4.5" />
+          )}{' '}
         </button>
+
         <div>{result?.번역_내용}</div>
       </div>
       <ActionButton onClick={playAudio} text="AI 음성으로 듣기" invert={true} />
