dd76f96db271526233e27a64a68b86f4a68ccb89
imddoy <imddoy@duksung.ac.kr>
2026-01-26 01:00:29 +0900

    HN-95-feat/쿠폰 상세 조회 action 구현

--- hanaro-pass/app/coupons/(coupon-header)/[id]/page.tsx
+++ hanaro-pass/app/coupons/(coupon-header)/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { getCouponById } from '../../actions/getCoupon';
 import CouponDetail from '../../components/coupon/CouponDetail';
 
 type PageProps = {
@@ -7,20 +8,14 @@ type PageProps = {
 export default async function Page({ params }: PageProps) {
   const { id } = await params;
 
-  const data = {
-    brandPic:
-      'https://blog.kakaocdn.net/dna/lMgCJ/btqVvPDO1IB/AAAAAAAAAAAAAAAAAAAAAMppshZ7hQfAA8C0R-uK8w62V9O4BJYwNvifeBrHKjK8/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1769871599&allow_ip=&allow_referer=&signature=rXZhIFRLxIpT%2FxAqf7MMHNb%2Bdgc%3D',
-    brandName: '스타벅스',
-    tag: '#커피 전문점',
-    couponNumber: `HN-${id}-001234`,
-  };
+  const coupon = await getCouponById({ id });
 
   return (
     <CouponDetail
-      brandPic={data.brandPic}
-      brandName={data.brandName}
-      tag={data.tag}
-      couponNumber={data.couponNumber}
+      brandPic={coupon.brandPic}
+      brandName={coupon.brandName}
+      tag={coupon.tag ?? ''}
+      couponNumber={coupon.couponCode}
     />
   );
 }
