ed19b3c5bc9a16a02229882b94c7805bb4bfbf94
imddoy <imddoy@duksung.ac.kr>
2026-01-26 15:45:42 +0900

    HN-105-feat/코드리뷰 반영

--- hanaro-pass/app/(main)/components/Pay.tsx
+++ hanaro-pass/app/(main)/components/Pay.tsx
@@ -64,7 +64,7 @@ export default function Pay({ cardsPromise, couponList }: PayProps) {
 
       <MenuList type="pay" />
 
-      {pendingCardId && (
+      {pendingCardId !== null && (
         <PinInput
           onSuccessAction={() => handlePinSuccess(pendingCardId)}
           onCloseAction={() => setPendingCardId(null)}
