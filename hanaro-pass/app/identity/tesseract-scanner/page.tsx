ebbce27fd2cfc7c46c1b8e4bd36b477e5d59d1fa
thisminji <thisminji@dgu.ac.kr>
2026-01-27 11:32:36 +0900

    HN-110-fix/tesseractScanner test 파일 삭제

--- hanaro-pass/app/identity/tesseract-scanner/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/identity/tesseract-scanner/page.tsx
-import TesseractScanner from '../components/TesseractScanner';
-
-export default function TesseractScannerPage() {
-  return <TesseractScanner />;
-}
