ea1d14c00486f2b32eb88d71eb8df3aabe05c831
thisminji <thisminji@dgu.ac.kr>
2026-01-27 16:25:55 +0900

    HN-110-fix/ 조건부 렌더링 추가

--- hanaro-pass/app/identity/components/MobileQr.tsx
+++ hanaro-pass/app/identity/components/MobileQr.tsx
@@ -129,7 +129,7 @@ export default function MobileQr({ type, data }: MobileQrProps) {
             <div className="flex w-full items-center justify-between rounded-2xl bg-gray-50 px-6 py-5 text-gray-800">
               <span className="sm font-regular">Passport Number</span>
               <span className="font-bold text-green-ez text-xl tracking-tight">
-                {data.passportNumber}
+                {data.passportNumber || '-'}
               </span>
             </div>
           </div>
@@ -139,7 +139,9 @@ export default function MobileQr({ type, data }: MobileQrProps) {
             <div className="flex h-48 flex-1 flex-col justify-center space-y-4 rounded-2xl bg-gray-50 px-4 py-2 text-gray-800">
               <div>
                 <p className="mb-1 font-semibold text-xl">Status</p>
-                <p className="font-regular text-sm">{data.residenceStatus}</p>
+                <p className="font-regular text-sm">
+                  {data.residenceStatus || '-'}
+                </p>
               </div>
 
               <div className="h-px w-full bg-gray-200" />
