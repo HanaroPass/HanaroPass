16c39c742a1751fccefa639ea0817a026ec4499d
seyoung <20200927@sungshin.ac.kr>
2026-01-29 01:18:08 +0900

    HN-101-refactor/코드 리팩토링

--- hanaro-pass/app/medical/symptoms/analyze/page.tsx
+++ hanaro-pass/app/medical/symptoms/analyze/page.tsx
@@ -4,18 +4,14 @@ import Image from 'next/image';
 import ActionButton from '@/components/ui/ActionButton';
 import AnalysisTip from '../../components/symptom/AnalysisTip';
 import SymptomRadioGroup from '../../components/symptom/SymptomRadioGroup';
+import { useImageUpload } from '../../hooks/useImageUpload';
 import useSymptomResult from '../../hooks/useSymptomResult';
 
-export default function SymptomAnalyzeContent() {
-  const {
-    images,
-    imageUrls,
-    isLoading,
-    isImageCntOK,
-    handleImages,
-    handleSubmit,
-    clearImages,
-  } = useSymptomResult();
+export default function SymptomAnalyzePage() {
+  const { images, imageUrls, isImageCntOK, handleImages, clearImages } =
+    useImageUpload();
+
+  const { isLoading, handleSubmit } = useSymptomResult(images);
 
   return (
     <>
