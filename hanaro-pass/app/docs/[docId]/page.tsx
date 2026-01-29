01fc8e2a1ee6cfb5b831ca5fcf36972da02c1352
somi4219 <somi4219@gmail.com>
2026-01-29 16:52:14 +0900

    HN-157-fix:스크롤 막아두기

--- hanaro-pass/app/docs/[docId]/page.tsx
+++ hanaro-pass/app/docs/[docId]/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { DOCS_CARD_ITEMS } from '../constants/docsCardItem';
 import Header from '@/components/header/Header';
 import { getUserDocs } from '../actions/userDocs';
@@ -19,7 +18,7 @@ export default async function DocsDetailPage({ params }: DocsProps) {
   const fileUrl = userDoc?.fileUrl ?? '';
 
   return (
-    <>
+    <div className="flex h-dvh flex-col overflow-hidden bg-green-400">
       <Header title={doc?.title ?? '서류'} />
       <main className="min-h-dvh bg-green-400 pt-25">
         {/* 중앙 카드 */}
@@ -49,6 +48,6 @@ export default async function DocsDetailPage({ params }: DocsProps) {
           title={doc?.title ?? '서류'}
         />
       </main>
-    </>
+    </div>
   );
 }
