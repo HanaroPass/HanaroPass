74e8299324343ab266f421bf262a15f1743e7930
hansoojeongsj <tnwjd3758@gmail.com>
2026-01-31 17:27:12 +0900

    HN-172-fix: 알럿 버튼도 밝게 했습니다.

--- hanaro-pass/app/docs/[docId]/docsPreviewClient.tsx
+++ hanaro-pass/app/docs/[docId]/docsPreviewClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Image from 'next/image';
 import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 import { useEffect, useMemo, useState } from 'react';
 
 type Props = {
